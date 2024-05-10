d7a7f3c44e7d8df8d80e541644dc7e1c0a779537
Jaya <jaya.lohana24@gmail.com>
2024-05-10 14:27:24 -0400

    changes

--- sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -40,6 +40,14 @@ export default class SortingVisualizer extends React.Component {
             }}
           ></div>
         ))}
+        <button onClick={() => this.resetArray()}>Generate New Array</button>
+        <button onClick={() => this.mergeSort()}>Merge Sort</button>
+        <button onClick={() => this.quickSort()}>Quick Sort</button>
+        <button onClick={() => this.heapSort()}>Heap Sort</button>
+        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+        <button onClick={() => this.testSortingAlgorithms()}>
+          Test Sorting Algorithms (BROKEN)
+        </button>
       </div>
     );
   }
