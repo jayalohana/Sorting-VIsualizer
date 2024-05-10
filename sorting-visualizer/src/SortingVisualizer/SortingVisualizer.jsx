7e78ba1e30982d837d71b079bb55e921f64be520
Jaya <jaya.lohana24@gmail.com>
2024-05-10 12:29:45 -0400

    changes

--- sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -16,7 +16,7 @@ export default class SortingVisualizer extends React.Component {
 
   resetArray() {
     const array = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 310; i++) {
       array.push(this.randomIntFromInterval(5, 1000));
     }
     this.setState({ array }); // Correct the way to update the state
@@ -30,13 +30,17 @@ export default class SortingVisualizer extends React.Component {
   render() {
     const { array } = this.state;
     return (
-      <>
+      <div className="array-container">
         {array.map((value, idx) => (
-          <div className="array-bar" key={idx}>
-            {value}
-          </div>
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              height: `${value}px`,
+            }}
+          ></div>
         ))}
-      </>
+      </div>
     );
   }
 }
